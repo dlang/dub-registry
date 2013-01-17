2abf78585ba601d73bb2ce57b54f558ce437139c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-01-17 08:45:39 +0100

    Dates are now formatted in a nicer way and the time of day is dropped.

---
+++ source/viewutils.d
@@ -0,0 +1,17 @@
+module viewutils;
+
+import std.datetime;
+import std.string;
+import vibe.data.json;
+
+string formatDate(string iso_ext_date)
+{
+	auto date = SysTime.fromISOExtString(iso_ext_date);
+	return (cast(Date)date).toSimpleString();
+}
+
+string formatDate(Json date)
+{
+	if( date.type == Json.Type.Undefined ) return "---";
+	return formatDate(date.get!string);
+}
