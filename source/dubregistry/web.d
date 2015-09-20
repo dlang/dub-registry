4a8cebea217ec4454e850b6f364decb76a7f86ea
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-09-20 21:00:43 +0200

    Switch default documented package recipe format to SDL.

--- source/dubregistry/web.d
+++ source/dubregistry/web.d
@@ -128,7 +128,7 @@ class DubRegistryWebFrontend {
 	void getPackageFormat(string lang = null)
 	{
 		switch (lang) {
-			default: redirect("package-format?lang=json"); break;
+			default: redirect("package-format?lang=sdl"); break;
 			case "json": render!("package_format_json.dt"); break;
 			case "sdl": render!("package_format_sdl.dt"); break;
 		}
