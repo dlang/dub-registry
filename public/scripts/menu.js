bc28dbf589fdeb6b0092707dd051a7e132ab35da
Martin Nowak <code@dawg.eu>
2016-03-22 08:40:55 +0100

    expand/focus search-container, use preventDefault, ignore main bar clicks
    - expand/focus search container which is used for narrow screens
    - return false didn't work for search, replace it w/ preventDefault
    - don't hide unfolded menu for clicks on main bar
    
    squash! expand search-container and use preventDefault

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -14,24 +14,34 @@ if (typeof cssmenu_no_js === 'undefined') {
             open_main_item = container.classList.contains('open') ? container: null;
         }
         e.stopPropagation();
-        return false;
+        e.preventDefault();
     }
 
     // menu button for mobile devices
     var dom_hamburger = document.body.querySelector(".hamburger.expand-toggle");
-    dom_hamburger.addEventListener('click', function(evt){
-        return handleMenuClick(dom_hamburger.parentNode, evt, true);
+    dom_hamburger.addEventListener('click', function(e){
+        handleMenuClick(dom_hamburger.parentNode, e, true);
+    });
+
+    // search container narrow screens
+    var dom_search = document.body.querySelector(".search-container > .expand-toggle");
+    dom_search.addEventListener('click', function(e){
+        handleMenuClick(dom_search.parentNode, e, false);
+        document.body.querySelector("#search-query > input").focus();
     });
 
     var expandToggles = document.body.querySelectorAll("#cssmenu .expand-toggle");
     // HTMLCollections don't expose a forEach
     [].forEach.call(expandToggles, function(expandToggle){
-        expandToggle.addEventListener("click", function(e) {
-            return handleMenuClick(expandToggle.parentNode, e, false);
+        expandToggle.addEventListener('click', function(e) {
+            handleMenuClick(expandToggle.parentNode, e, false);
         });
     });
 
-    // close window on clicks to other regions
+    // close menu when clicking outside of the main bar
+    document.getElementById('top').addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
     window.addEventListener("click", function(e) {
         if (open_main_item !== null) {
             open_main_item.classList.remove("open");
