dlang/dub-registry

commit 54c4a524f5995d0c5149baa42facab3d6731a166

Add safe annotations and upgrade to latest vibe.d pre-release.
- Uses "safe" annotations more or less throughout the code base
- By using the latest vibe.d alpha, fixes MongoDB aggregations with MongoDB 3.6 series
- Fixes all errors and deprecation warnings

source/dubregistry/repositories/gitlab.d
@@ -20,6 +20,8 @@ import vibe.textfilter.urlencode;
 
 
 class GitLabRepository : Repository {
+@safe:
+
 	private {
 		string m_owner;
 		string m_project;
@@ -29,7 +31,7 @@ class GitLabRepository : Repository {
 
 	static void register(string auth_token, string url)
 	{
-		Repository factory(DbRepository info){
+		Repository factory(DbRepository info) @safe {
 			return new GitLabRepository(info.owner, info.project, auth_token, url.length ? URL(url) : URL("https://gitlab.com/"));
 		}
 		addRepositoryFactory("gitlab", &factory);
@@ -90,7 +92,7 @@ class GitLabRepository : Repository {
 		return ret;
 	}
 
-	void readFile(string commit_sha, Path path, scope void delegate(scope InputStream) reader)
+	void readFile(string commit_sha, InetPath path, scope void delegate(scope InputStream) @safe reader)
 	{
 		assert(path.absolute, "Passed relative path to readFile.");
 		auto url = m_baseURL.toString() ~ (m_owner ~ "/" ~ m_project ~ "/raw/" ~ commit_sha) ~ path.toString() ~ "?private_token="~m_authToken;
@@ -101,21 +103,23 @@ class GitLabRepository : Repository {
 
 	string getDownloadUrl(string ver)
 	{
+		import std.uri : encodeComponent;
 		if (m_authToken.length > 0) return null; // don't make private token public
 		if( ver.startsWith("~") ) ver = ver[1 .. $];
 		else ver = ver;
-		return m_baseURL.toString()~m_owner~"/"~m_project~"/repository/archive.zip?ref="~ver;
+		auto venc = () @trusted { return encodeComponent(ver); } ();
+		return m_baseURL.toString()~m_owner~"/"~m_project~"/repository/archive.zip?ref="~venc;
 	}
 
-	void download(string ver, scope void delegate(scope InputStream) del)
+	void download(string ver, scope void delegate(scope InputStream) @safe del)
 	{
-		if( ver.startsWith("~") ) ver = ver[1 .. $];
-		else ver = ver;
-		auto url = m_baseURL.toString()~m_owner~"/"~m_project~"/repository/archive.zip?ref="~ver~"&private_token="~m_authToken;
+		auto url = getDownloadUrl(ver);
+		url ~= "&private_token="~m_authToken;
 		downloadCached(url, del);
 	}
 
-	private string getAPIURLPrefix() {
+	private string getAPIURLPrefix()
+	{
 		return m_baseURL.toString() ~ "api/v3/projects/" ~ (m_owner ~ "/" ~ m_project).urlEncode ~ "/";
 	}
 }

dub.sdl
@@ -4,8 +4,8 @@ homepage "http://code.dlang.org/"
 authors "Sönke Ludwig" "Matthias Dondorff"
 license "GPL-3.0"
 
-dependency "vibe-d" version="~>0.8.0-beta"
-dependency "dub" version="~>1.3.0-beta"
+dependency "vibe-d" version="~>0.8.3-alpha"
+dependency "dub" version="~>1.7.0"
 dependency "userman" version="~>0.3.2"
 subConfiguration "dub" "library-nonet"
 

source/app.d
@@ -68,7 +68,7 @@ shared static this()
 	}
 
 	import dub.internal.utils : jsonFromFile;
-	auto regsettingsjson = jsonFromFile(Path("settings.json"), true);
+	auto regsettingsjson = jsonFromFile(NativePath("settings.json"), true);
 	auto ghuser = regsettingsjson["github-user"].opt!string;
 	auto ghpassword = regsettingsjson["github-password"].opt!string;
 	auto glurl = regsettingsjson["gitlab-url"].opt!string;
@@ -92,10 +92,10 @@ shared static this()
 		// user management
 		auto udbsettings = new UserManSettings;
 		udbsettings.serviceName = "DUB - The D package registry";
-		udbsettings.serviceUrl = URL("http://code.dlang.org/");
+		udbsettings.serviceURL = URL("http://code.dlang.org/");
 		udbsettings.serviceEmail = "noreply@vibed.org";
 		udbsettings.databaseURL = "mongodb://127.0.0.1:27017/vpmreg";
-		udbsettings.requireAccountValidation = false;
+		udbsettings.requireActivation = false;
 		userdb = createUserManController(udbsettings);
 	}
 

source/dubregistry/api.d
@@ -35,11 +35,11 @@ alias registerDubRegistryWebApi = registerDubRegistryAPI;
 private void dumpPackages(HTTPServerRequest req, HTTPServerResponse res, DubRegistry registry)
 {
 	import vibe.data.json : serializeToPrettyJson;
-	import vibe.stream.wrapper : StreamOutputRange;
+	import vibe.stream.wrapper : streamOutputRange;
 
 	res.contentType = "application/json; charset=UTF-8";
 	res.headers["Content-Encoding"] = "gzip"; // force GZIP compressed response
-	auto dst = StreamOutputRange(res.bodyWriter);
+	auto dst = streamOutputRange(res.bodyWriter);
 	dst.put('[');
 	bool first = true;
 	foreach (p; registry.getPackageDump()) {
@@ -73,6 +73,8 @@ struct SearchResult { string name, description, version_; }
 struct DownloadStats { DbDownloadStats downloads; }
 
 interface IPackages {
+@safe:
+
 	@method(HTTPMethod.GET)
 	SearchResult[] search(string q = "");
 
@@ -158,8 +160,10 @@ override {
 }
 
 private:
-	string rootOf(string pkg) {
+	string rootOf(string pkg) @safe {
 		import std.algorithm: findSplitBefore;
+		// FIXME: urlDecode should not be necessary, as the REST paramters are
+		//        already decoded.
 		return pkg.urlDecode().findSplitBefore(":")[0];
 	}
 }

source/dubregistry/cache.d
@@ -6,6 +6,7 @@
 module dubregistry.cache;
 
 import vibe.core.log;
+import vibe.core.stream;
 import vibe.db.mongo.mongo;
 import vibe.http.client;
 import vibe.stream.memory;
@@ -24,6 +25,7 @@ enum CacheMatchMode {
 
 
 class URLCache {
+@safe:
 	private {
 		MongoClient m_db;
 		MongoCollection m_entries;
@@ -42,16 +44,17 @@ class URLCache {
 		m_entries.remove(["url": url.toString()]);
 	}
 
-	void get(URL url, scope void delegate(scope InputStream str) callback, bool cache_priority = false)
+	void get(URL url, scope void delegate(scope InputStream str) @safe callback, bool cache_priority = false)
 	{
 		get(url, callback, cache_priority ? CacheMatchMode.always : CacheMatchMode.etag);
 	}
 
-	void get(URL url, scope void delegate(scope InputStream str) callback, CacheMatchMode mode = CacheMatchMode.etag)
+	void get(URL url, scope void delegate(scope InputStream str) @safe callback, CacheMatchMode mode = CacheMatchMode.etag)
 	{
 		import std.datetime : Clock, UTC;
 		import vibe.http.auth.basic_auth;
 		import dubregistry.internal.utils : black;
+		import vibe.internal.interfaceproxy : asInterface;
 
 		auto user = url.username;
 		auto password = url.password;
@@ -70,7 +73,7 @@ class URLCache {
 				// invalidate out of date cache entries
 				if (be["_id"].get!BsonObjectID.timeStamp < now - m_maxCacheTime)
 					m_entries.remove(["_id": be["_id"]]);
-				
+
 				deserializeBson(entry, be);
 				if (mode == CacheMatchMode.always) {
 					// directly return cache result for cache_priority == true
@@ -80,7 +83,8 @@ class URLCache {
 						continue;
 					} else {
 						auto data = be["data"].get!BsonBinData().rawData();
-						scope tmpresult = createMemoryStream(cast(ubyte[])data, false);
+						auto mdata = () @trusted { return cast(ubyte[])data; } ();
+						scope tmpresult = createMemoryStream(mdata, false);
 						callback(tmpresult);
 						return;
 					}
@@ -138,7 +142,7 @@ class URLCache {
 
 							logDiagnostic("Cache MISS (no etag): %s", url.toString());
 							handled_uncached = true;
-							callback(res.bodyReader);
+							callback(res.bodyReader.asInterface!InputStream);
 							break;
 					}
 				}
@@ -173,24 +177,24 @@ private struct CacheEntry {
 
 private URLCache s_cache;
 
-void downloadCached(URL url, scope void delegate(scope InputStream str) callback, bool cache_priority = false)
-{
+void downloadCached(URL url, scope void delegate(scope InputStream str) @safe callback, bool cache_priority = false)
+@safe {
 	if (!s_cache) s_cache = new URLCache;
 	s_cache.get(url, callback, cache_priority);
 }
 
-void downloadCached(string url, scope void delegate(scope InputStream str) callback, bool cache_priority = false)
-{
+void downloadCached(string url, scope void delegate(scope InputStream str) @safe callback, bool cache_priority = false)
+@safe {
 	return downloadCached(URL.parse(url), callback, cache_priority);
 }
 
 void clearCacheEntry(URL url)
-{
+@safe {
 	if (!s_cache) s_cache = new URLCache;
 	s_cache.clearEntry(url);
 }
 
 void clearCacheEntry(string url)
-{
+@safe {
 	clearCacheEntry(URL(url));
 }

source/dubregistry/dbcontroller.d
@@ -16,6 +16,8 @@ import vibe.vibe;
 
 
 class DbController {
+@safe:
+
 	private {
 		MongoCollection m_packages;
 		MongoCollection m_downloads;
@@ -36,14 +38,14 @@ class DbController {
 			bool any_change = false;
 			if (p["branches"].type == Bson.Type.object) {
 				Bson[] branches;
-				foreach( b; p["branches"] )
+				foreach (b; p["branches"].byValue)
 					branches ~= b;
 				p["branches"] = branches;
 				any_change = true;
 			}
 			if (p["branches"].type == Bson.Type.array) {
 				auto versions = p["versions"].get!(Bson[]);
-				foreach (b; p["branches"]) versions ~= b;
+				foreach (b; p["branches"].byValue) versions ~= b;
 				p["branches"] = Bson(null);
 				p["versions"] = Bson(versions);
 				any_change = true;
@@ -189,9 +191,11 @@ class DbController {
 			new_versions.sort!((a, b) => vcmp(a, b));
 
 			// remove versions with invalid dependency names to avoid the findAndModify below to fail
-			new_versions = new_versions.filter!(
+			() @trusted {
+				new_versions = new_versions.filter!(
 					v => !v.info["dependencies"].opt!(Json[string]).byKey.canFind!(k => k.canFind("."))
 				).array;
+			} ();
 
 			//assert((cast(Json)bversions).toString() == (cast(Json)serializeToBson(versions)).toString());
 
@@ -326,7 +330,7 @@ class DbController {
 	DbStatDistributions getStatDistributions()
 	{
 		auto aggregate(T, string prefix, string groupBy)()
-		{
+		@safe {
 			auto group = ["_id": Bson(groupBy ? "$"~groupBy : null)];
 			Bson[string] project;
 			foreach (mem; __traits(allMembers, T))
@@ -354,7 +358,7 @@ class DbController {
 			else
 			{
 				T[string] ret;
-				foreach (doc; res)
+				foreach (doc; res.byValue)
 					ret[doc["_id"].get!string] = doc.deserializeBson!T;
 				return ret;
 			}
@@ -462,18 +466,18 @@ struct DbStatDistributions {
 }
 
 bool vcmp(DbPackageVersion a, DbPackageVersion b)
-{
+@safe {
 	return vcmp(a.version_, b.version_);
 }
 
 bool vcmp(string va, string vb)
-{
+@safe {
 	import dub.dependency;
 	return Version(va) < Version(vb);
 }
 
 private string[] splitAlphaNumParts(string str)
-{
+@safe {
 	string[] ret;
 	while (!str.empty) {
 		while (!str.empty && !str.front.isIdentChar()) str.popFront();
@@ -494,6 +498,6 @@ private string[] splitAlphaNumParts(string str)
 }
 
 private bool isIdentChar(dchar ch)
-{
+@safe {
 	return std.uni.isAlpha(ch) || std.uni.isNumber(ch);
 }

source/dubregistry/internal/utils.d
@@ -3,13 +3,13 @@ module dubregistry.internal.utils;
 import vibe.inet.url;
 
 URL black(URL url)
-{
+@safe {
 	if (url.username.length > 0) url.username = "***";
 	if (url.password.length > 0) url.password = "***";
 	return url;
 }
 
 string black(string url)
-{
+@safe {
 	return black(URL(url)).toString();
 }

source/dubregistry/internal/workqueue.d
@@ -14,6 +14,8 @@ import vibe.core.sync;
 import vibe.utils.array : FixedRingBuffer;
 
 final class PackageWorkQueue {
+@safe:
+
 	private {
 		FixedRingBuffer!string m_queue;
 		string m_current;
@@ -24,7 +26,7 @@ final class PackageWorkQueue {
 		SysTime m_lastSignOfLifeOfUpdateTask;
 	}
 
-	this(void delegate(string) handler)
+	this(void delegate(string) @safe handler)
 	{
 		m_handler = handler;
 		m_queue.capacity = 10000;
@@ -86,7 +88,7 @@ final class PackageWorkQueue {
 			try m_handler(pack);
 			catch (Exception e) {
 				logWarn("Failed to handle package %s: %s", pack, e.msg);
-				logDiagnostic("Full error: %s", e.toString().sanitize);
+				() @trusted { logDiagnostic("Full error: %s", e.toString().sanitize); } ();
 			}
 		}
 	}

commit 10d44a396a5ef51ad4b6b748eb13857d3cd05a29

Fix forwarded download of GitLab hosted packages.
When authorization was used, the URL used by download() was invalid, because getDownloadURL() returned null in this case. This was a regression introduced in 54c4a524f5995d0c5149baa42facab3d6731a166.

source/dubregistry/repositories/gitlab.d
@@ -103,19 +103,24 @@ class GitLabRepository : Repository {
 
 	string getDownloadUrl(string ver)
 	{
-		import std.uri : encodeComponent;
-		if (m_authToken.length > 0) return null; // don't make private token public
-		if( ver.startsWith("~") ) ver = ver[1 .. $];
-		else ver = ver;
-		auto venc = () @trusted { return encodeComponent(ver); } ();
-		return m_baseURL.toString()~m_owner~"/"~m_project~"/repository/archive.zip?ref="~venc;
+		if (m_authToken.length > 0) return null; // public download URL doesn't work
+		return getRawDownloadURL(ver);
 	}
 
 	void download(string ver, scope void delegate(scope InputStream) @safe del)
 	{
-		auto url = getDownloadUrl(ver);
+		auto url = getRawDownloadURL(ver);
 		url ~= "&private_token="~m_authToken;
 		downloadCached(url, del);
+	}
+
+	private string getRawDownloadURL(string ver)
+	{
+		import std.uri : encodeComponent;
+		if (ver.startsWith("~")) ver = ver[1 .. $];
+		else ver = ver;
+		auto venc = () @trusted { return encodeComponent(ver); } ();
+		return m_baseURL.toString()~m_owner~"/"~m_project~"/repository/archive.zip?ref="~venc;
 	}
 
 	private string getAPIURLPrefix()