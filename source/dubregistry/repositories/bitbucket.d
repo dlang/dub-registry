a47ac1fae50d889ff17164e0cb1e6c3faf6d5bb4
Radu Racariu <radur@j2inn.com>
2018-06-28 20:10:17 +0300

    add bitbucket authentication

--- source/dubregistry/repositories/bitbucket.d
+++ source/dubregistry/repositories/bitbucket.d
@@ -22,26 +22,30 @@ class BitbucketRepository : Repository {
 	private {
 		string m_owner;
 		string m_project;
+		string m_authUser;
+		string m_authPassword;
 	}
 
-	static void register()
+	static void register(string user, string password)
 	{
 		Repository factory(DbRepository info) @safe {
-			return new BitbucketRepository(info.owner, info.project);
+			return new BitbucketRepository(info.owner, info.project, user, password);
 		}
 		addRepositoryFactory("bitbucket", &factory);
 	}
 
-	this(string owner, string project)
+	this(string owner, string project, string auth_user, string auth_password)
 	{
 		m_owner = owner;
 		m_project = project;
+		m_authUser = auth_user;
+		m_authPassword = auth_password;
 	}
 
 	RefInfo[] getTags()
 	{
 		Json tags;
-		try tags = readJson("https://api.bitbucket.org/1.0/repositories/"~m_owner~"/"~m_project~"/tags");
+		try tags = readJson(getAPIURLPrefix ~ "/1.0/repositories/"~m_owner~"/"~m_project~"/tags");
 		catch( Exception e ) { throw new Exception("Failed to get tags: "~e.msg); }
 		RefInfo[] ret;
 		foreach (string tagname, tag; tags.byKeyValue) {
@@ -59,7 +63,7 @@ class BitbucketRepository : Repository {
 
 	RefInfo[] getBranches()
 	{
-		Json branches = readJson("https://api.bitbucket.org/1.0/repositories/"~m_owner~"/"~m_project~"/branches");
+		Json branches = readJson(getAPIURLPrefix ~ "/1.0/repositories/"~m_owner~"/"~m_project~"/branches");
 		RefInfo[] ret;
 		foreach (string branchname, branch; branches.byKeyValue) {
 			auto commit_hash = branch["raw_node"].get!string();
@@ -72,7 +76,7 @@ class BitbucketRepository : Repository {
 
 	RepositoryInfo getInfo()
 	{
-		auto nfo = readJson("https://api.bitbucket.org/1.0/repositories/"~m_owner~"/"~m_project);
+		auto nfo = readJson(getAPIURLPrefix ~ "/1.0/repositories/"~m_owner~"/"~m_project);
 		RepositoryInfo ret;
 		ret.isFork = nfo["is_fork"].opt!bool;
 		ret.stats.watchers = nfo["followers_count"].opt!uint;
@@ -83,7 +87,7 @@ class BitbucketRepository : Repository {
 	RepositoryFile[] listFiles(string commit_sha, InetPath path)
 	{
 		assert(path.absolute, "Passed relative path to listFiles.");
-		auto url = "https://bitbucket.org/api/2.0/repositories/"~m_owner~"/"~m_project~"/src/"~commit_sha~path.toString()~"?pagelen=100";
+		auto url = getAPIURLPrefix ~ "/api/2.0/repositories/"~m_owner~"/"~m_project~"/src/"~commit_sha~path.toString()~"?pagelen=100";
 		auto ls = readJson(url)["values"].get!(Json[]);
 		RepositoryFile[] ret;
 		ret.reserve(ls.length);
@@ -108,7 +112,7 @@ class BitbucketRepository : Repository {
 	void readFile(string commit_sha, InetPath path, scope void delegate(scope InputStream) @safe reader)
 	{
 		assert(path.absolute, "Passed relative path to readFile.");
-		auto url = "https://bitbucket.org/api/1.0/repositories/"~m_owner~"/"~m_project~"/raw/"~commit_sha~path.toString();
+		auto url = getAPIURLPrefix ~ "/1.0/repositories/"~m_owner~"/"~m_project~"/raw/"~commit_sha~path.toString();
 		downloadCached(url, (scope input) @safe {
 			reader(input);
 		}, true);
@@ -120,12 +124,21 @@ class BitbucketRepository : Repository {
 		if( ver.startsWith("~") ) ver = ver[1 .. $];
 		else ver = ver;
 		auto venc = () @trusted { return encodeComponent(ver); } ();
-		return "https://bitbucket.org/"~m_owner~"/"~m_project~"/get/"~venc~".zip";
+		const url = "https://bitbucket.org/"~m_owner~"/"~m_project~"/get/"~venc~".zip";  
+		if (m_authUser.length) return "https://"~encodeComponent(m_authUser)~":"~encodeComponent(m_authPassword)~"@"~url["https://".length..$];
+		return url;
 	}
 
 	void download(string ver, scope void delegate(scope InputStream) @safe del)
 	{
 		downloadCached(getDownloadUrl(ver), del);
+	}
+
+	private string getAPIURLPrefix()
+	{
+		import std.uri : encodeComponent;
+		if (m_authUser.length) return "https://"~encodeComponent(m_authUser)~":"~encodeComponent(m_authPassword)~"@api.bitbucket.org";
+		else return "https://api.bitbucket.org";
 	}
 }
 
