af333592eb28c831d7d8e4216185031c3d4f2d12
Mathias LANG <pro.mathias.lang@gmail.com>
2022-10-19 14:08:18 +0100

    dbcontroller: Remove old migration code (#532)

--- source/dubregistry/dbcontroller.d
+++ source/dubregistry/dbcontroller.d
@@ -38,53 +38,10 @@ class DbController {
 		// migrations:
 		//
 
-		version (DubRegistry_EnableLegacyMigrations) {
-			// update package format
-			foreach(p; m_packages.find()){
-				bool any_change = false;
-				if (p["branches"].type == Bson.Type.object) {
-					Bson[] branches;
-					foreach (b; p["branches"].byValue)
-						branches ~= b;
-					p["branches"] = branches;
-					any_change = true;
-				}
-				if (p["branches"].type == Bson.Type.array) {
-					auto versions = p["versions"].get!(Bson[]);
-					foreach (b; p["branches"].byValue) versions ~= b;
-					p["branches"] = Bson(null);
-					p["versions"] = Bson(versions);
-					any_change = true;
-				}
-				if (any_change) m_packages.update(["_id": p["_id"]], p);
-			}
-
-			// add updateCounter field for packages that don't have it yet
-			m_packages.update(["updateCounter": ["$exists": false]], ["$set" : ["updateCounter" : 0L]], UpdateFlags.multiUpdate);
-		}
-
-		// add default non-@optional stats to packages
-		DbPackageStats stats;
-		m_packages.update(["stats": ["$exists": false]], ["$set": ["stats": stats]], UpdateFlags.multiUpdate);
-
-		// rename stats.rating -> stats.score
-		m_packages.update(Bson.emptyObject(), ["$rename": ["stats.rating": "stats.score"]], UpdateFlags.multiUpdate);
-
-		// default initialize missing scores with zero
-		float score = 0;
-		m_packages.update(["stats.score": ["$exists": false]], ["$set": ["stats.score": score]], UpdateFlags.multiUpdate);
-
-		// remove old logo fields
-		m_packages.update(["logoHash": ["$exists": true]], ["$unset": ["logo": 0, "logoHash": 0]], UpdateFlags.multiUpdate);
-
 		// create indices
 		m_packages.ensureIndex([tuple("name", 1)], IndexFlags.Unique);
 		m_packages.ensureIndex([tuple("stats.score", 1)]);
 		m_downloads.ensureIndex([tuple("package", 1), tuple("version", 1)]);
-
-		// drop old text index versions
-		db.runCommand(["dropIndexes": "packages", "index": "packages_full_text_search_index"]);
-		db.runCommand(["dropIndexes": "packages", "index": "packages_full_text_search_index_v2"]);
 
 		// add current text index
 		immutable keyWeights = [
@@ -108,14 +65,6 @@ class DbController {
 		cmd["indexes"] = [Bson(fts)];
 		auto res = db.runCommand(cmd);
 		enforce(res["ok"].opt!double == 1.0, "Failed to create search index.\n"~res.toString);
-
-		version (DubRegistry_RepairVersionOrder) {
-			// sort package versions newest to oldest
-			// NOTE: since quite a while, versions are inserted atomically
-			//       in the proper order, so that this is not necessary as a
-			//       general precaution anymore
-			repairVersionOrder();
-		}
 	}
 
 	void addPackage(ref DbPackage pack)
