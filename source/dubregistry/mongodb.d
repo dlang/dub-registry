d8807a865ad195f23075934ca511c1a3e52fc847
Mathias Lang <mlang@symmetryinvestments.com>
2023-06-25 17:58:25 +0200

    Check the result of parseMongoDBUrl with enforce

--- source/dubregistry/mongodb.d
+++ source/dubregistry/mongodb.d
@@ -4,6 +4,7 @@ import vibe.core.log;
 import vibe.db.mongo.client : MongoClient;
 import vibe.db.mongo.mongo : connectMongoDB;
 import vibe.db.mongo.settings : MongoClientSettings, MongoAuthMechanism, parseMongoDBUrl;
+import std.exception : enforce;
 import std.typecons : Nullable;
 
 string databaseName = "vpmreg";
@@ -17,7 +18,9 @@ MongoClientSettings mongoSettings() {
 		auto mongodbURI = environment.get("MONGODB_URI", environment.get("MONGO_URI", "mongodb://127.0.0.1"));
 		logInfo("Found mongodbURI: %s", mongodbURI);
 		MongoClientSettings settings;
-		parseMongoDBUrl(settings, mongodbURI);
+		enforce(parseMongoDBUrl(settings, mongodbURI),
+				"Could not parse connection string (check MONGODB_URI or MONGO_URI): "
+				~ mongodbURI);
 		settings.authMechanism = MongoAuthMechanism.scramSHA1;
 		if (settings.database.length > 0)
 			databaseName = settings.database;
